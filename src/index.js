8367b9d230322e5348e92a248b27c3bc8dd3bf09
sakshilambe1997 <sakshilambe1997@gmail.com>
2024-10-31 15:10:47 +0530

    home page added

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Home from "./views/Home/Home"
+import BlogView from "./views/BlogView/BlogView"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router =createBrowserRouter([
+  {
+    path:"/",
+    element:<Home/>
+  },
+  {
+    path:"blog",
+    element:<BlogView/>
+  },
+])
+root.render(<RouterProvider router ={router}/>);
+
+
---
+++ src/views/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1 className='heading'>LED TV blogs</h1>
+      
+    </div>
+  )
+}
+
+export default Home
